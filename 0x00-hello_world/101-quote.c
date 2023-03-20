1a78000830d4cf1c7313e3dd1b761f85ba54e41f
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-20 02:29:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include<unistd.h>
-/** main- prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * followed by anew line to the standard error
- * returns 1
+/** main- prints and that piece of art is useful - Dora Korpar, * followed by anew line to the standard error
+ * always returns 1
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
