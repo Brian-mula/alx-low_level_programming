3329239c70837d8ef63ed227c1daea400825f39a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-20 04:16:41 -0700

    ./100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,13 +3,13 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Entrypoint
  * Return: Always 0 (Success)
  **/
 
 int main(void)
 {
-	int d,p;
+	int d, p;
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
@@ -19,11 +19,10 @@ int main(void)
 		putchar(d);
 		putchar(p);
 		if (d == '8' && p == '9')
-		{
 		continue;
 		putchar(',');
 		putchar(' ');
-		}
+		
 		}
 		}
 	
