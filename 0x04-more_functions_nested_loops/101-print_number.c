57c5fb451997177b280115dc39c48e83003465d9
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-23 06:56:08 -0700

    ./101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,13 +10,16 @@
 void print_number(int n)
 {
 	unsigned int k = n;
+
 	if (n < 0)
 	{
 	n += -1;
 	k = n;
+
 	_putchar('_');
 	}
 	k /= 10;
+
 	if (k != 0)
 	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
