18844f01d9309303d39dfd22894556760a3c4b71
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-03 02:37:24 -0700

    ./7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - for printing the chessboard
+ * @a: printed chessboard
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int firstindex, secondindex;
+
+	for (firstindex = 0; a[firstindex][7]; firstindex++)
+	{
+		for (secondindex = 0; secondindex < 8; secondindex++)
+			_putchar(a[firstindex][secondindex]);
+		
+		_putchar('\n');
+	}
+}
