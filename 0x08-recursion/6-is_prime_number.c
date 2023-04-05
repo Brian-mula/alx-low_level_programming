15d22b388c1f0405188a12970fec2867f50eea49
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-05 11:01:25 -0700

    ./6-prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * prime - to find prime
+ * @j: int param
+ * @k: int param
+ * Return: bool
+ */
+
+int prime(int j, int k)
+{
+	if (k % j == 0)
+	{
+		return (0);
+	}
+	else if (k / 2 > j)
+	{
+		return (prime(j + 2, k));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+
+
+
+/**
+ * is_prime_number - to find prime number
+ * @n: the value tested
+ * Return: prime number
+ *
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(3,n));
+	}
+}
