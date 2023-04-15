cd4cd6b3bb23a6555420a2a60fffbe1bbde41e90
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-15 10:32:03 -0700

    ./mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - for printing its name
+*@argc: the argument count
+*@argv: argument variable
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
