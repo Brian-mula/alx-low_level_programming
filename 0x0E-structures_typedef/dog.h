dd32ddac316bf3ed5d5194507bc93759354a21c0
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-17 02:47:14 -0700

    upfated the comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - represents dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+
+
+
 struct dog{
 	char *name;
 	float age;
